02896312439fb91a244c22802798b832f5f51391
Andrei Lima <andrei.lima75@gmail.com>
2026-04-24 16:29:37 -0300

    streams poc

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,12 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String name;
     private int age;
--- src/main/java/org/example/StreamsPOC.java
+++ src/main/java/org/example/StreamsPOC.java
@@ -1,6 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class StreamsPOC {
 
-    static void main() {}
+    static void main() {
+        List<Person> people = Arrays.asList(
+                new Person("Alice", 25, "Female"),
+                new Person("Bob", 30, "Male"),
+                new Person("Charlie", 17, "Male"),
+                new Person("Diana", 35, "Female"),
+                new Person("Eve", 22, "Female")
+        );
+
+        System.out.println("=== Pessoas originais ===");
+        people.forEach(System.out::println);
+
+        Predicate<Person> isAdult = p -> p.getAge() >= 18;
+
+
+        System.out.println("=== 1. Filter: Adultos ===");
+
+        List<Person> adults = people.stream()
+                .filter(isAdult)
+                .toList();
+
+        adults.forEach(System.out::println);
+    }
 }
